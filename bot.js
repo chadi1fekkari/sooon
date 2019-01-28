b7c2de22511a8884a0b34115870316e72feac237
chadi1fekkari <40345231+chadi1fekkari@users.noreply.github.com>
2019-01-28 23:27:44 +0000

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,81 @@
+const Discord = require("discord.js");
+
+const client = new Discord.Client();
+
+client.on('ready', () => {
+
+  console.log(`Logged in as ${client.user.tag} !`);
+
+ 
+
+});
+
+ 
+
+var PrEfix = "-";
+
+client.on('message', message => {
+
+  if (!message.content.startsWith(PrEfix)) return;
+
+  var args = message.content.split(' ').slice(1);
+
+  var argresult = args.join(' ');
+
+  if (message.author.id !== '501428682783457280') return;
+
+if (message.content.startsWith(PrEfix + 'wt')) {
+
+client.user.setActivity(argresult, {type:'WATCHING'});
+
+    message.channel.sendMessage(`**✅  : ${argresult}**`)
+
+} else 
+
+if (message.content.startsWith(PrEfix + 'ls')) {
+
+client.user.setActivity(argresult, {type:'LISTENING'});
+
+    message.channel.sendMessage(`**✅  : ${argresult}**`)
+
+} else 
+
+if (message.content.startsWith(PrEfix + 'st')) {
+
+  client.user.setGame(argresult, "https://www.twitch.tv/amira");
+
+    message.channel.sendMessage(`**✅  : ${argresult}**`)
+
+}
+
+});
+
+client.on('message', message => {
+
+if(message.content.startsWith('c')) {
+
+if(message.author.id !== '501428682783457280') return;
+
+var args = message.content.split(' ').slice(1).join(' ');
+
+message.channel.send(args);
+
+}
+
+});
+
+client.on('message', message => {
+
+if(message.content.startsWith('c')) {
+
+if(message.author.id !== '501428682783457280') return;
+
+var args = message.content.split(' ').slice(1).join(' ');
+
+message.channel.send(args);
+
+}
+
+});
+
+client.login(process.env.BOT_TOKEN);
